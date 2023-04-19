25ab9fb408655669720b756f5977102017f26095
LimEunSang <dmstkd2905@naver.com>
2023-04-19 22:08:46 +0900

    ~ 페이지 레이아웃 만들기
    JSX 문법 숙달을 위한 실습 예제 몇 가지 작성

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div></div>;
+  let name = "park";
+  return (
+    <div>
+      <h4 className="title" style={{ color: "red", fontSize: "30px" }}>
+        애플후레시
+      </h4>
+      <p className="title-sub">by dev {name}</p>
+    </div>
+  );
 }
