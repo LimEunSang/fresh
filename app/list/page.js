58915b4f60a975bc1764abd4f3c1b4ed08dd03b9
LimEunSang <dmstkd2905@naver.com>
2023-04-19 22:56:10 +0900

    ~ 여러 페이지 만들기
    - 라우팅 페이지 생성
    - 해더 생성

---
+++ app/list/page.js
@@ -0,0 +1,15 @@
+const List = () => {
+  return (
+    <>
+      <h4 className="title">상품 목록</h4>
+      <div className="food">
+        <h4>상품1 $40</h4>
+      </div>
+      <div className="food">
+        <h4>상품2 $40</h4>
+      </div>
+    </>
+  );
+};
+
+export default List;
