84f3dff116b724a5fb523fc3a1416175aa80d98c
LimEunSang <dmstkd2905@naver.com>
2023-04-19 21:29:40 +0900

    Revert "~ 페이지 레이아웃 만들기"
    This reverts commit b4323e8262e3c61892fe76205cfd080899492689.

--- app/page.js
+++ app/page.js
@@ -1,11 +1,3 @@
 export default function Home() {
-  let name = "park";
-  return (
-    <div>
-      <h4 className="title" style={{ color: "red", fontSize: "30px" }}>
-        애플후레시
-      </h4>
-      <p className="title-sub">by dev {name}</p>
-    </div>
-  );
+  return <div></div>;
 }
