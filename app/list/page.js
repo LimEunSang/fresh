48336e64ae16fbbd4d5b6e9fe8b027dc2591a991
LimEunSang <dmstkd2905@naver.com>
2023-04-20 11:37:48 +0900

    ~ Next.js에서 이미지 넣는 법 2개
    - 이미지 추가 및 사용
    - cart 페이지 생성

--- app/list/page.js
+++ app/list/page.js
@@ -1,11 +1,15 @@
+import Image from "next/image";
+import food0 from "/public/food0.png";
+
 const List = () => {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
 
   return (
     <>
       <h4 className="title">상품 목록</h4>
-      {상품.map((item) => (
-        <div className="food" key={item}>
+      {상품.map((item, key) => (
+        <div className="food" key={key}>
+          <img src={`/food${key}.png`} className="food-img" />
           <h4>{item} $40</h4>
         </div>
       ))}
