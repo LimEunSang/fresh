039ec94d81baaddaf20fc785662e20f7a822ea86
LimEunSang <dmstkd2905@naver.com>
2023-04-20 11:02:26 +0900

    ~ html을 반복문으로 줄이고 싶으면 map
    map() 사용하여 상품 목록 출력

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,14 @@
 const List = () => {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+
   return (
     <>
       <h4 className="title">상품 목록</h4>
-      <div className="food">
-        <h4>상품1 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품2 $40</h4>
-      </div>
+      {상품.map((item) => (
+        <div className="food" key={item}>
+          <h4>{item} $40</h4>
+        </div>
+      ))}
     </>
   );
 };
