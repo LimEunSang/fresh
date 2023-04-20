192c66351d05990d754860f93ccd2b1e109fe99d
LimEunSang <dmstkd2905@naver.com>
2023-04-20 12:09:37 +0900

    ~ client/server component, import 문법
    - cartItem 컴포넌트화
    - import, export 예제 연습

---
+++ app/cart/data.js
@@ -0,0 +1,5 @@
+let age = 20;
+let name = "kim";
+
+// export default age;
+export { age, name };
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,17 +1,21 @@
+import { age, name } from "./data";
+
+const CartItem = () => {
+  return (
+    <div className="cart-item">
+      <p>상품명 {age}</p>
+      <p>$40 {name}</p>
+      <p>1개</p>
+    </div>
+  );
+};
+
 const Cart = () => {
   return (
     <>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem />
+      <CartItem />
     </>
   );
 };
