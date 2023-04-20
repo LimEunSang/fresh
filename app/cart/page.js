85cb1bb4573d169a8d530723eceac65fb121ddcb
LimEunSang <dmstkd2905@naver.com>
2023-04-20 12:33:42 +0900

    ~ Component에 데이터 전해주려면 props
    부모 -> 자식 컴포넌트 데이터 전달 연습

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,22 +1,21 @@
-import { age, name } from "./data";
-
-const CartItem = () => {
-  return (
-    <div className="cart-item">
-      <p>상품명 {age}</p>
-      <p>$40 {name}</p>
-      <p>1개</p>
-    </div>
-  );
-};
-
 const Cart = () => {
+  let 장바구니 = ["Tomatoes", "Pasta"];
   return (
     <>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]} />
     </>
+  );
+};
+
+const CartItem = ({ item }) => {
+  return (
+    <div className="cart-item">
+      <p>{item}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
   );
 };
 
